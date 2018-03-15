ea3cee28e7687253144aeac6303cf747a1a03e5f
Juan D. Gaitan <jdgaitanf@unal.edu.co>
2018-03-15 15:56:12 -0500

    Validaciones, parcialmente

--- backend/app/models/calificacion.rb
+++ backend/app/models/calificacion.rb
@@ -1,5 +1,16 @@
 class Calificacion < ApplicationRecord
-     belongs_to :pedido
-     has_many :foto
-     has_one :usuario
+    belongs_to :pedido
+    has_many :fotos
+    has_one :usuario
+
+    validates_associated :fotos
+    validates_associated :usuario
+
+    validates :id, presence: true
+    validates :idcalificador, presence: true
+    validates :idventa, presence: true
+    validates :idcalificado, presence: true
+    validates :comentario, allow_blank: true
+    validates :id, numericality: { message: "%{value} parece no ser un número" }
+    validates :prestigio, numericality: { message: "%{value} parece no ser un número" }
 end
--- backend/app/models/foto.rb
+++ backend/app/models/foto.rb
@@ -1,3 +1,12 @@
 class Foto < ApplicationRecord
     belongs_to :producto
+
+    validates :id, presence: true
+    validates :id, numericality: { message: "%{value} parece no ser un número" }
+    validates :fecha, presence: true
+    validates :idventa, presence: true
+    validates :foto, presence: true
+    validates :producto, allow_blank: true
+    
+ 
 end
