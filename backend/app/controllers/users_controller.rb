893fa9f188ccea5a79d6d2f4eba32e48e9a9b9a6
Felipe Ramos <cframosl@unal.edu.co>
2018-03-30 00:09:16 +0000

    Primera version de Gema Faker, todavia falta implementarla en otros modelos

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:type, :iddocument, :typedocument, :email, :phone, :latitude, :langitude, :password)
+      params.require(:user).permit(:typeuser, :iddocument, :typedocument, :email, :phone, :latitude, :langitude, :password)
     end
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,93 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+include Faker
+
+#10.times do 
+#    Classification.create(
+#        name: Faker::Name.name
+#    )
+#end
+User.destroy_all
+
+5.times do 
+    User.create(
+        name: Faker::Name.name,
+        lastname: Faker::Name.last_name,
+        typeuser: "comprador",
+        iddocument: Faker::Code.imei,
+        typedocument: "CC",
+        email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.phone_number,
+        latitude: Faker::Number.decimal(2, 3),
+        langitude: Faker::Number.decimal(2, 3),
+        password: Faker::Internet.password(8)
+    )
+end
+
+5.times do 
+    User.create(
+        name: Faker::Name.name,
+        lastname: Faker::Name.last_name,
+        typeuser: "vendedor",
+        iddocument: Faker::Code.imei,
+        typedocument: "CC",
+        email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.phone_number,
+        latitude: Faker::Number.decimal(2, 3),
+        langitude: Faker::Number.decimal(2, 3),
+        password: Faker::Internet.password(8)
+    )
+end
+
+2.times do 
+    User.create(
+        name: Faker::Name.name,
+        lastname: Faker::Name.last_name,
+        typeuser: "administrador",
+        iddocument: Faker::Code.imei,
+        typedocument: "CC",
+        email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.phone_number,
+        latitude: Faker::Number.decimal(2, 3),
+        langitude: Faker::Number.decimal(2, 3),
+        password: Faker::Internet.password(8)
+    )
+end
+
+Classification.destroy_all
+
+3.times do 
+    Classification.create(
+        name: Faker::Name.name
+    )
+end
+
+Article.destroy_all
+
+10.times do 
+    Article.create(
+        name: Faker::Commerce.product_name,
+        description: Faker::Lorem.paragraph,
+        price: Faker::Number.decimal(6, 3),
+        inventory: Faker::Number.number(4),
+        user_id: Faker::Number.between(1, 10),
+        classification_id: Number.between(1, 3)
+    )
+end
+
+
+
+#Alliance.destroy_all
+#
+#10.times do 
+#    Alliance.create(
+#        commentary: Faker::Lorem.paragraph,
+#        confirm: Faker::String.random(2),
+#        approval_id: Faker::Number.number(10),
+#        applicant_id: Faker::Number.number(10)
+#    )
+#end
+
