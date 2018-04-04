d9aa73bbf6e483a6f9188a56c60b791f388adf8a
CARLOS <canietot@unal.edu.co>
2018-04-04 12:41:33 -0500

    Se agregan querries al modelo Alliance

--- backend/app/models/alliance.rb
+++ backend/app/models/alliance.rb
@@ -18,6 +18,22 @@ class Alliance < ApplicationRecord
   validates :commentary, presence: true, length: { minimum: 5, maximum: 500 }
   validates :confirm, length: { minimum: 2, maximum: 8 }
   
+  #///////// Querries /////////
+  #En la siguiente sección se implementaran todos los queries de este modelo (métodos y scope).
+
+  #Este query nos devuelve cuantas alianzas fueron confirmadas.
+  #query en proceso
+
+  #Este query nos devuelve cuantas alianzas no fueron confirmadas.
+  #query en proceso
+
+  #Este query nos devuelve las alianzas creadas después de una fecha.
+  scope :created_before, ->(time) { where("created_at <= ?", time)}
+  #Este query nos devuelve las alianzas creadas antes de una fecha.
+  scope :created_after, ->(time) { where("created_at > ?", time)}
+  #Para obtener alianzas entre un rango de fechas, solo se necesita anidar los 2 queries anteriores.
+
+
   def self.withsale
     joins("
     INNER JOIN users  userapproval  ON alliances.approval_id  = userapproval.id 
