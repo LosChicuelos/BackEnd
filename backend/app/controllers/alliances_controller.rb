0751b0b0e5a603e70c4018ca27ed471a154e5536
Bryam Diaz <bsdiaza@unal.edu.co>
2018-05-24 22:20:36 +0000

    Listado de alianzas, solicitud de nuevas alianzas, integracion en el menu principal- parte del back

--- backend/app/controllers/alliances_controller.rb
+++ backend/app/controllers/alliances_controller.rb
@@ -1,9 +1,14 @@
 class AlliancesController < ApplicationController
-  before_action :set_alliance, only: [:show, :update, :destroy]
+  #before_action :set_alliance, only: [:show, :update, :destroy]
 
   # GET /alliances
   def index
     @alliances = Alliance.paginatedef(params[:page])
+    render json: @alliances
+  end
+  
+  def belongsuser
+    @alliances = Alliance.belongsuser(params[:user_id])
     render json: @alliances
   end
 
@@ -23,12 +28,15 @@ class AlliancesController < ApplicationController
 
   # POST /alliances
   def create
+    puts 'Hola'
+    puts alliance_params
+    puts 'hola2'
     @alliance = Alliance.new(alliance_params)  
     
     if @alliance.save
-      redirect_to @alliance
-    else
-      render :new
+      render json: @alliance, status: :created, location: @alliance
+      else
+      render json: @alliance.errors, status: :unprocessable_entity
     end
   end
 
@@ -55,6 +63,6 @@ class AlliancesController < ApplicationController
   private
     
     def alliance_params
-      params.require(:alliance).permit(:commentary, :confirm)
+      params.require(:alliance).permit(:commentary, :confirm, :approval_id, :applicant_id)
     end
 end
